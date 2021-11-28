d3aa30fe41600d80c41bfa279e245c8067c836ca
Dan Koenig <dan.koenig@helloworld.com>
2021-11-27 21:13:41 -0500

    fix add navigator, RigManagerm, ThirdPerson

--- src/classes/Environment3d.js
+++ src/classes/Environment3d.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { Object3D } from 'three';
 import {getRaycastIntersections, object3DSelector} from '../utils/THREEHelpers.js';
 export default class Environment3d{
     constructor(element, { width, height, background }){
@@ -6,10 +7,14 @@ export default class Environment3d{
         const _height = height ? height :  700;
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( background | 0xFFFFFF );
+        this.cameraContainer = new Object3D();
         this.camera = new THREE.PerspectiveCamera( 75, _width / _height, 0.1, 1000 );
+        this.cameraContainer.add(this.camera);
+        this.scene.add(this.cameraContainer);
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize( _width, _height );
         element.appendChild(this.renderer.domElement);
+        console.log(this.cameraContainer);
     }
     render(){
         this.renderer.render(this.scene, this.camera);
